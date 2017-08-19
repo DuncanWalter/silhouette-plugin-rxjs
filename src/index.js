fc5dd311cfb47064d460840a968e2394f070e786
DuncanWalter <dwalter@barlingarts.com>
2017-08-19 12:01:32 -0500

    updating to match sil-core changes and imporving deploment process

--- src/index.js
+++ src/index.js
@@ -1,45 +1,45 @@
 import { BehaviorSubject } from 'rxjs/BehaviorSubject'
 
+// TODO schedule pushes for after reducer returns
+
 // non colliding instance props are important 
 const __stream__ = Symbol('stream'); 
 
+import { symbols } from 'silhouette-core'
+
 // middleware for plugging in rxjs- not too difficult, 
 // even with lazy BehaviorSubjects for performance
-export default settings => { return {
-    Silhouette(next, namespace){
-
-        let symbols = namespace.symbols;
-        let proto = next.prototype;
-        
-        // State changes result in push calls which are designed to
-        // be hijacked for reactive purposes.
-        let push = proto[symbols.__push__];
-        proto[symbols.__push__] = function({ value, done }){
-            push({ value, done });
-            if(this[__stream__] instanceof BehaviorSubject){
-                if(done){
-                    this[__stream__].complete();
+export default function(settings){ 
+    return {
+        prototype: {
+            // State changes result in push calls which are designed to
+            // be hijacked for reactive purposes.
+            [symbols.__push__]: next => function({ value, done }){
+                next.call(this, { value, done });
+                if(this[__stream__] instanceof BehaviorSubject){
+                    if(done){
+                        this[__stream__].complete();
+                    } else {
+                        this[__stream__].next(value);
+                    }
                 } else {
-                    this[__stream__].next(value);
+                    this[__stream__] = value;
                 }
-            } else {
-                this[__stream__] = value;
-            }
-            if(done){
-                Object.keys(this).forEach(key => this[key][symbols.__push__]({ done }));
-            }
+                // shuts down child nodes upon stream termination here
+                if(done){
+                    Reflect.ownKeys(this).forEach(key => this[key][symbols.__push__]({ done }));
+                }
+            },
+            // we need to add a method for accessing the streams as an outsider.
+            // could also use defineProperty and get to make it fancy, but
+            // I think that might cut against the grain of RXJS philosophy? Not sure.
+            asObservable: next => function(){
+                if( next ){ throw new Error('RXJS plugin cannot overwrite asObservable properties defined by other plugins')}
+                if(!(this[__stream__] instanceof BehaviorSubject)){
+                    this[__stream__] = new BehaviorSubject(this[__stream__]);
+                }
+                return this[__stream__].asObservable();
+            },
         }
-
-        // we need to add a method for accessing the streams as an outsider.
-        // could also use defineProperty and get to make it fancy, but
-        // I think that might cut against the grain of RXJS philosophy? Not sure.
-        proto.asObservable = function(){
-            if(!(this[__stream__] instanceof BehaviorSubject)){
-                this[__stream__] = new BehaviorSubject(this[__stream__]);
-            }
-            return this[__stream__].asObservable();
-        }
-
-        return next;
     }
-}};
+};
